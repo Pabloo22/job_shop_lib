f078fa56b5b92808dd76e3abc77ef3a19964cca4
Pabloo22 <pablete.arino@gmail.com>
2024-02-12 11:30:23 +0100

    Refactor imports in __init__.py

--- job_shop_lib/__init__.py
+++ job_shop_lib/__init__.py
@@ -1,2 +1,8 @@
-from .operation import Operation
-from .job_shop_instance import JobShopInstance
+from job_shop_lib.operation import Operation
+from job_shop_lib.job_shop_instance import JobShopInstance
+
+
+__all__ = [
+    "Operation",
+    "JobShopInstance",
+]
