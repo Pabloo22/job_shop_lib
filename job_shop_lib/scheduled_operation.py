acd5a55bd15fffea4f4f486308160d313b530af3
Pabloo22 <pablete.arino@gmail.com>
2024-02-13 14:34:06 +0100

    Add ScheduledOperation class

---
+++ job_shop_lib/scheduled_operation.py
@@ -0,0 +1,21 @@
+from job_shop_lib import Operation
+
+
+class ScheduledOperation:
+
+    __slots__ = ("operation", "start_time", "machine_id")
+
+    def __init__(self, operation: Operation, start_time: int, machine_id: int):
+        if machine_id not in operation.machines:
+            raise ValueError(
+                f"Operation cannot be scheduled on machine {machine_id}. "
+                f"Valid machines are {operation.machines}."
+            )
+        
+        self.operation = operation
+        self.start_time = start_time
+        self.machine_id = machine_id
+
+    @property
+    def end_time(self) -> int:
+        return self.start_time + self.operation.duration
