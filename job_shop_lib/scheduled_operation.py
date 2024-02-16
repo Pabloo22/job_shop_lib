20eb7cc780d984711f83f1f7b91abca5677ce5ec
Pabloo22 <pablete.arino@gmail.com>
2024-02-16 18:25:55 +0100

    Refactor machine_id property in ScheduledOperation class

--- job_shop_lib/scheduled_operation.py
+++ job_shop_lib/scheduled_operation.py
@@ -3,18 +3,25 @@ from job_shop_lib import Operation
 
 class ScheduledOperation:
 
-    __slots__ = ("operation", "start_time", "machine_id")
+    __slots__ = ("operation", "start_time", "_machine_id")
 
     def __init__(self, operation: Operation, start_time: int, machine_id: int):
-        if machine_id not in operation.machines:
-            raise ValueError(
-                f"Operation cannot be scheduled on machine {machine_id}. "
-                f"Valid machines are {operation.machines}."
-            )
-
         self.operation = operation
         self.start_time = start_time
-        self.machine_id = machine_id
+        self._machine_id = machine_id
+
+    @property
+    def machine_id(self) -> int:
+        return self._machine_id
+
+    @machine_id.setter
+    def machine_id(self, value: int):
+        if value not in self.operation.machines:
+            raise ValueError(
+                f"Operation cannot be scheduled on machine {value}. "
+                f"Valid machines are {self.operation.machines}."
+            )
+        self._machine_id = value
 
     @property
     def end_time(self) -> int:
