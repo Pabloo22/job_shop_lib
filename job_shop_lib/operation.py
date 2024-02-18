2c788d035801e183a116a46b388b76f1f01c2c8b
Pabloo22 <pablete.arino@gmail.com>
2024-02-18 16:59:22 +0100

    Add job_id and position attributes to Operation class

--- job_shop_lib/operation.py
+++ job_shop_lib/operation.py
@@ -1,14 +1,24 @@
 """Home of the Operation class."""
 
+from typing import Optional
+
 
 class Operation:
     """Stores machine and duration information for a job operation."""
 
-    __slots__ = ("machines", "duration")
+    __slots__ = ("machines", "duration", "job_id", "position")
 
-    def __init__(self, machines: int | list[int], duration: int):
+    def __init__(
+        self,
+        machines: int | list[int],
+        duration: int,
+        job_id: Optional[int] = None,
+        position: Optional[int] = None,
+    ):
         self.machines = [machines] if isinstance(machines, int) else machines
         self.duration = duration
+        self.job_id = job_id
+        self.position = position
 
     @property
     def machine_id(self) -> int:
@@ -18,9 +28,12 @@ class Operation:
             raise ValueError("Operation has multiple machines.")
         return self.machines[0]
 
-    def get_id(self, job_id: int, position: int) -> str:
+    @property
+    def operation_id(self) -> str:
         """Returns the id of the operation."""
-        return f"J{job_id}M{self.machine_id}P{position}"
+        if self.job_id is None or self.position is None:
+            raise ValueError("Operation has no job_id or position.")
+        return f"J{self.job_id}M{self.machine_id}P{self.position}"
 
     @staticmethod
     def get_job_id_from_id(op_id: str) -> int:
@@ -36,3 +49,12 @@ class Operation:
     def get_position_from_id(op_id: str) -> int:
         """Returns the position from the operation id."""
         return int(op_id.split("P")[1])
+
+    def __repr__(self) -> str:
+        machines = (
+            self.machines[0] if len(self.machines) == 1 else self.machines
+        )
+        return (
+            f"O(m={machines}, d={self.duration}, "
+            f"j={self.job_id}, p={self.position})"
+        )
