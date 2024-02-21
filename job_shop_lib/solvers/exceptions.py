14306eafae33b0ec3959e4bd643b92b2ba7ecc44
Pabloo22 <pablete.arino@gmail.com>
2024-02-21 16:19:12 +0100

    Refactor NoSolutionFound exception

--- job_shop_lib/solvers/exceptions.py
+++ job_shop_lib/solvers/exceptions.py
@@ -1,8 +1,3 @@
 class NoSolutionFound(Exception):
     """Exception raised when no solution is found."""
 
-    def __init__(
-        self, message="No solution could be found for the given problem"
-    ):
-        self.message = message
-        super().__init__(self.message)
