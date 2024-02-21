3f4a51545def2270a456bbbb218e0193f7f0cfbe
Pabloo22 <pablete.arino@gmail.com>
2024-02-21 16:19:37 +0100

    Fix Dispatcher class

--- job_shop_lib/dispatcher.py
+++ job_shop_lib/dispatcher.py
@@ -13,6 +13,7 @@ class Dispatcher:
         self.schedule = Schedule(self.instance)
         self.machine_next_available_time = [0] * self.instance.num_machines
         self.job_next_operation_index = [0] * self.instance.num_jobs
+        self.job_next_available_time = [0] * self.instance.num_jobs
 
     def reset(self) -> None:
         self.schedule = Schedule(self.instance)
@@ -23,18 +24,29 @@ class Dispatcher:
         if not self.is_operation_ready(operation):
             raise ValueError("Operation is not ready to be scheduled.")
 
-        timestep = self.machine_next_available_time[machine_id]
+        start_time = self.compute_start_time(operation, machine_id)
 
         scheduled_operation = ScheduledOperation(
-            operation, machine_id, timestep
+            operation, start_time, machine_id
         )
-        self.schedule.dispatch(scheduled_operation)
+        self.schedule.add(scheduled_operation)
 
-        self.machine_next_available_time[machine_id] += operation.duration
+        self.machine_next_available_time[machine_id] = (
+            start_time + operation.duration
+        )
         self.job_next_operation_index[operation.job_id] += 1
+        self.job_next_available_time[operation.job_id] = (
+            start_time + operation.duration
+        )
 
     def is_operation_ready(self, operation: Operation) -> bool:
         return (
             self.job_next_operation_index[operation.job_id]
             == operation.position_in_job
+        )
+
+    def compute_start_time(self, operation: Operation, machine_id: int) -> int:
+        return max(
+            self.machine_next_available_time[machine_id],
+            self.job_next_available_time[operation.job_id],
         )
