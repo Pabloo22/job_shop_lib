e2bc6c77c67bc146a36a8ea4e909f91f05b9d460
Pabloo22 <pablete.arino@gmail.com>
2024-02-21 22:40:04 +0100

    Add dispatching_rule_solver to solvers module

--- job_shop_lib/solvers/__init__.py
+++ job_shop_lib/solvers/__init__.py
@@ -1,8 +1,10 @@
 from job_shop_lib.solvers.exceptions import NoSolutionFound
 from job_shop_lib.solvers.cp_solver import CPSolver
+from job_shop_lib.solvers.dispatching_rules import dispatching_rule_solver
 
 
 __all__ = [
     "NoSolutionFound",
     "CPSolver",
+    "dispatching_rule_solver",
 ]
