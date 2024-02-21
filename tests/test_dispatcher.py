d3bbb53243dbe4ae70ba7f90cb8810108a710ffb
Pabloo22 <pablete.arino@gmail.com>
2024-02-21 19:18:37 +0100

    Add tests for Dispatcher class

---
+++ tests/test_dispatcher.py
@@ -0,0 +1,54 @@
+from job_shop_lib import Dispatcher, JobShopInstance
+
+
+def test_dispatch(example_job_shop_instance: JobShopInstance):
+    dispatcher = Dispatcher(example_job_shop_instance)
+
+    job_1 = example_job_shop_instance.jobs[0]
+    job_2 = example_job_shop_instance.jobs[1]
+    job_3 = example_job_shop_instance.jobs[2]
+
+    machine_1 = 0
+    machine_2 = 1
+    machine_3 = 2
+
+    dispatcher.dispatch(job_1[0], machine_1)
+    dispatcher.dispatch(job_1[1], machine_2)
+    dispatcher.dispatch(job_3[0], machine_3)
+    dispatcher.dispatch(job_3[1], machine_1)
+    dispatcher.dispatch(job_2[0], machine_2)
+    dispatcher.dispatch(job_1[2], machine_3)
+    dispatcher.dispatch(job_3[2], machine_2)
+    dispatcher.dispatch(job_2[1], machine_3)
+    dispatcher.dispatch(job_2[2], machine_1)
+
+    assert dispatcher.schedule.makespan() == 11
+    assert dispatcher.job_next_available_time == [9, 11, 9]
+    assert dispatcher.machine_next_available_time == [11, 9, 10]
+    assert dispatcher.job_next_operation_index == [3, 3, 3]
+
+
+def test_reset(example_job_shop_instance: JobShopInstance):
+    dispatcher = Dispatcher(example_job_shop_instance)
+
+    job_1 = example_job_shop_instance.jobs[0]
+    job_2 = example_job_shop_instance.jobs[1]
+    job_3 = example_job_shop_instance.jobs[2]
+
+    machine_1 = 0
+    machine_2 = 1
+    machine_3 = 2
+
+    dispatcher.dispatch(job_1[0], machine_1)
+    dispatcher.dispatch(job_1[1], machine_2)
+    dispatcher.dispatch(job_3[0], machine_3)
+    dispatcher.dispatch(job_3[1], machine_1)
+    dispatcher.dispatch(job_2[0], machine_2)
+    dispatcher.dispatch(job_1[2], machine_3)
+
+    dispatcher.reset()
+
+    assert dispatcher.schedule.makespan() == 0
+    assert dispatcher.job_next_available_time == [0, 0, 0]
+    assert dispatcher.machine_next_available_time == [0, 0, 0]
+    assert dispatcher.job_next_operation_index == [0, 0, 0]
