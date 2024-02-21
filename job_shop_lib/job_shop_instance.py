2411bc7c0d8666c3ee8cc4a17389841a87794101
Pabloo22 <pablete.arino@gmail.com>
2024-02-21 16:53:38 +0100

    Refactor JobShopInstance class to improve readability and add documentation

--- job_shop_lib/job_shop_instance.py
+++ job_shop_lib/job_shop_instance.py
@@ -3,13 +3,13 @@
 from __future__ import annotations
 
 import functools
-from typing import Optional, Any
+from typing import Any
 
 from job_shop_lib import Operation
 
 
 class JobShopInstance:
-    """Stores a classic job-shop scheduling problem instance."""
+    """Data structure which stores a Job Shop Scheduling Problem instance."""
 
     def __init__(
         self,
@@ -38,25 +38,22 @@ class JobShopInstance:
         """Returns the number of jobs in the instance."""
         return len(self.jobs)
 
-    @property
-    def bounds(self) -> tuple[float | None, float | None]:
-        """Returns the lower and upper bounds of the instance."""
-        return self.lower_bound, self.upper_bound
+    @functools.cached_property
+    def num_machines(self) -> int:
+        """Returns the number of machines in the instance.
 
-    @property
-    def upper_bound(self) -> Optional[float]:
-        """Returns the upper bound of the instance."""
-        return self.metadata.get("upper_bound")
+        Computed as the maximum machine id present in the instance plus one.
+        """
+        mx = 0
+        for job in self.jobs:
+            mx_machine = max(max(operation.machines) for operation in job)
+            mx = max(mx, mx_machine)
+        return mx + 1
 
-    @property
-    def lower_bound(self) -> Optional[float]:
-        """Returns the lower bound of the instance."""
-        return self.metadata.get("lower_bound")
-
-    @property
-    def optimum(self) -> Optional[float]:
-        """Returns the optimum of the instance."""
-        return self.metadata.get("optimum")
+    @functools.cached_property
+    def num_operations(self) -> int:
+        """Returns the number of operations in the instance."""
+        return sum(len(job) for job in self.jobs)
 
     @functools.cached_property
     def is_flexible(self) -> bool:
@@ -74,7 +71,21 @@ class JobShopInstance:
 
     @functools.cached_property
     def machines_matrix(self) -> list[list[list[int]]] | list[list[int]]:
-        """Returns the machines matrix of the instance."""
+        """Returns the machines matrix of the instance.
+
+        If the instance is flexible (i.e., if any operation has more than one
+        machine in which it can be processed), the returned matrix is a list of
+        lists of lists of integers.
+
+        Otherwise, the returned matrix is a list of lists of integers.
+
+        To access the machines of the operation with id i in the job with id j,
+        the following code must be used:
+        ```python
+        machines = instance.machines_matrix[j][i]
+        ```
+
+        """
         if self.is_flexible:
             return [
                 [operation.machines for operation in job] for job in self.jobs
@@ -84,34 +95,10 @@ class JobShopInstance:
         ]
 
     @functools.cached_property
-    def job_durations(self) -> list[float]:
-        """Returns the duration of each job in the instance."""
-        return [
-            sum(operation.duration for operation in job) for job in self.jobs
-        ]
-
-    @functools.cached_property
-    def total_duration(self) -> float:
-        """Returns the total duration of the instance."""
-        return sum(self.job_durations)
-
-    @functools.cached_property
-    def num_machines(self) -> int:
-        """Returns the number of machines in the instance."""
-        mx = 0
-        for job in self.jobs:
-            mx_machine = max(max(operation.machines) for operation in job)
-            mx = max(mx, mx_machine)
-        return mx + 1
-
-    @functools.cached_property
-    def num_operations(self) -> int:
-        """Returns the number of operations in the instance."""
-        return sum(len(job) for job in self.jobs)
-
-    @functools.cached_property
     def max_duration(self) -> float:
-        """Returns the maximum duration of the instance."""
+        """Returns the maximum duration of the instance.
+
+        Useful for normalizing the durations of the operations."""
         mx = 0
         for job in self.jobs:
             mx_duration = max(operation.duration for operation in job)
@@ -119,30 +106,31 @@ class JobShopInstance:
         return mx
 
     @functools.cached_property
-    def max_job_duration(self) -> float:
-        """Returns the maximum duration of a job in the instance."""
-        return max(
+    def job_durations(self) -> list[float]:
+        """Returns a list with the duration of each job in the instance.
+
+        The duration of a job is the sum of the durations of its operations.
+
+        The duration of the job with id i is stored in the i-th position of the
+        returned list.
+        """
+        return [
             sum(operation.duration for operation in job) for job in self.jobs
-        )
+        ]
 
     @functools.cached_property
     def machine_loads(self) -> list[int]:
-        """Returns the total duration of each machine in the instance."""
+        """Returns the total machine load of each machine in the instance.
+
+        The total machine load of a machine is the sum of the durations of the
+        operations that can be processed in that machine.
+
+        The total machine load of the machine with id i is stored in the i-th
+        position of the returned list.
+        """
         machine_times = [0 for _ in range(self.num_machines)]
         for job in self.jobs:
             for operation in job:
                 for machine_id in operation.machines:
                     machine_times[machine_id] += operation.duration
         return machine_times
-
-    @functools.cached_property
-    def max_machine_load(self) -> float:
-        """Returns the maximum duration of a machine in the instance."""
-        return max(self.machine_loads)
-
-    @functools.cached_property
-    def mean_machine_load(self) -> float:
-        """Returns the mean duration of a machine in the instance."""
-        return self.total_duration / self.num_machines
-
-    # -------------------
