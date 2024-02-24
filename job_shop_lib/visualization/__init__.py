10b38d720b8e147e5ed68743f75848101c2e3984
Pabloo22 <pablete.arino@gmail.com>
2024-02-24 12:10:36 +0100

    Add create_gif function and related changes

--- job_shop_lib/visualization/__init__.py
+++ job_shop_lib/visualization/__init__.py
@@ -1,4 +1,10 @@
 from job_shop_lib.visualization.gantt_chart import plot_gantt_chart
+from job_shop_lib.visualization.create_gif import (
+    create_gif,
+    create_gantt_chart_frames,
+    get_default_plot_function,
+    create_gif_from_frames,
+)
 
 
 __all__ = ["plot_gantt_chart"]
--- job_shop_lib/visualization/create_gif.py
+++ job_shop_lib/visualization/create_gif.py
@@ -1,74 +1,80 @@
 import os
 import pathlib
 import shutil
-from typing import Optional
+from typing import Optional, Callable
 
 import imageio
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 
-from job_shop_lib import JobShopInstance, Dispatcher
+from job_shop_lib import JobShopInstance, Dispatcher, Schedule
 from job_shop_lib.solvers import DispatchingRuleSolver
 from job_shop_lib.visualization import plot_gantt_chart
 
 
-def create_gif_from_dispatching_rule_solver(
+def get_default_plot_function(
+    title: Optional[str] = None, cmap: str = "viridis"
+) -> Callable[[Callable], Callable]:
+    def default_plot_function(schedule: Schedule, makespan: int) -> Figure:
+        fig, _ = plot_gantt_chart(
+            schedule, title=title, cmap_name=cmap, xlim=makespan
+        )
+        return fig
+
+    return default_plot_function
+
+
+def create_gif(
+    gif_path: str,
     instance: JobShopInstance,
     solver: DispatchingRuleSolver,
-    gif_path: str,
+    plot_function: Callable[[Schedule, int], Figure] = None,
     fps: int = 1,
     remove_frames: bool = True,
-    gantt_charts_title: Optional[str] = None,
-    gantt_charts_cmap: str = "viridis",
 ) -> None:
-    frames_dir = _create_directory_for_frames("frames")
-    _create_gantt_chart_frames(
-        instance, solver, frames_dir, gantt_charts_title, gantt_charts_cmap
-    )
-    _create_gif_from_frames(frames_dir, gif_path, fps)
+    if plot_function is None:
+        plot_function = get_default_plot_function()
+
+    path = pathlib.Path("temp_frames")
+    path.mkdir(exist_ok=True)
+    frames_dir = str(path)
+    create_gantt_chart_frames(frames_dir, instance, solver, plot_function)
+    create_gif_from_frames(frames_dir, gif_path, fps)
 
     if remove_frames:
         shutil.rmtree(frames_dir)
 
 
-def _create_directory_for_frames(dir_name: str) -> str:
-    path = pathlib.Path(dir_name)
-    path.mkdir(exist_ok=True)
-    return str(path)
-
-
-def _create_gif_from_frames(frames_dir: str, gif_path: str, fps: int) -> None:
-    frames = [
-        os.path.join(frames_dir, frame) for frame in os.listdir(frames_dir)
-    ]
-    images = [imageio.imread(frame) for frame in frames]
-    imageio.mimsave(gif_path, images, fps=fps)
-
-
-def _create_gantt_chart_frames(
+def create_gantt_chart_frames(
+    frames_dir: str,
     instance: JobShopInstance,
     solver: DispatchingRuleSolver,
-    frames_dir: str,
-    gantt_charts_title: Optional[str],
-    gantt_charts_cmap: str,
+    plot_function: Callable[[Schedule, int], Figure],
 ) -> None:
-    makespan = solver(instance).makespan()
     dispatcher = Dispatcher(instance)
     schedule = dispatcher.schedule
+    makespan = solver(instance).makespan()
     iteration = 0
 
-    fig, _ = plot_gantt_chart(schedule, xlim=makespan)
+    fig = plot_function(schedule, makespan)
     _save_frame(fig, frames_dir, iteration)
 
     while not schedule.is_complete():
         solver.step(dispatcher)
         iteration += 1
-        fig, _ = plot_gantt_chart(
-            schedule, gantt_charts_title, gantt_charts_cmap
-        )
+        fig = plot_function(schedule, makespan)
         _save_frame(fig, frames_dir, iteration)
 
 
 def _save_frame(figure: Figure, frames_dir: str, number: int) -> None:
     figure.savefig(f"{frames_dir}/frame_{number:02d}.png")
     plt.close(figure)
+
+
+def create_gif_from_frames(frames_dir: str, gif_path: str, fps: int) -> None:
+    frames = [
+        os.path.join(frames_dir, frame)
+        for frame in sorted(os.listdir(frames_dir))
+    ]
+    images = [imageio.imread(frame) for frame in frames]
+    imageio.mimsave(gif_path, images, fps=fps)
