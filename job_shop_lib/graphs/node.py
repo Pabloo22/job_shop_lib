076fb01d0df02b03b9c2c9b88fc8216b4f3e13f8
Pabloo22 <pablete.arino@gmail.com>
2024-02-27 00:08:13 +0100

    Fix Node class

--- job_shop_lib/graphs/node.py
+++ job_shop_lib/graphs/node.py
@@ -4,11 +4,13 @@ from dataclasses import dataclass, field
 from job_shop_lib.graphs.constants import NodeTypes, NODE_ID_DEFAULT
 
 
-@dataclass(slots=True, frozen=True)
+@dataclass(slots=True)
 class Node:
     node_type: NodeTypes
     value: Any = None
     node_id: int = field(default=NODE_ID_DEFAULT, init=False)
 
     def __hash__(self) -> int:
+        if self.node_id == NODE_ID_DEFAULT:
+            raise ValueError("Node has not been assigned an id.")
         return hash(self.node_id)
