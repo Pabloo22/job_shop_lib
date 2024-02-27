14ea166873013ca3468f0b14e8d4eeab9d17715d
Pabloo22 <pablete.arino@gmail.com>
2024-02-27 20:51:38 +0100

    Add disjunctive graph visualization

--- job_shop_lib/visualization/__init__.py
+++ job_shop_lib/visualization/__init__.py
@@ -5,7 +5,9 @@ from job_shop_lib.visualization.create_gif import (
     get_default_plot_function,
     create_gif_from_frames,
 )
-
+from job_shop_lib.visualization.disjunctive_graph import (
+    plot_classic_disjunctive_graph,
+)
 
 __all__ = [
     "plot_gantt_chart",
@@ -13,4 +15,5 @@ __all__ = [
     "create_gantt_chart_frames",
     "get_default_plot_function",
     "create_gif_from_frames",
+    "plot_classic_disjunctive_graph",
 ]
