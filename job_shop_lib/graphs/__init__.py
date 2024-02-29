e3e29284b28056615bf08ea810fddd78236c7985
Pabloo22 <pablete.arino@gmail.com>
2024-02-29 12:52:23 +0100

    Add new node types to job shop graphs

--- job_shop_lib/graphs/__init__.py
+++ job_shop_lib/graphs/__init__.py
@@ -1,5 +1,5 @@
 from job_shop_lib.graphs.constants import EdgeType, NodeType
-from job_shop_lib.graphs.node import Node
+from job_shop_lib.graphs.nodes import Node, OperationNode, MachineNode, JobNode
 from job_shop_lib.graphs.job_shop_graph import JobShopGraph
 from job_shop_lib.graphs.build_disjunctive_graph import (
     build_disjunctive_graph,
@@ -14,6 +14,9 @@ __all__ = [
     "EdgeType",
     "NodeType",
     "Node",
+    "OperationNode",
+    "MachineNode",
+    "JobNode",
     "JobShopGraph",
     "build_disjunctive_graph",
     "add_disjunctive_edges",
