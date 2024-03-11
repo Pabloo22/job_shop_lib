6f656379f4ed8129b01708a77d0f7737c1bbfa70
Pabloo22 <pablete.arino@gmail.com>
2024-03-11 13:17:39 +0100

    Add docstrings to Dispatcher class

--- job_shop_lib/dispatcher.py
+++ job_shop_lib/dispatcher.py
@@ -1,3 +1,5 @@
+"""Home of the Dispatcher class."""
+
 from job_shop_lib import (
     JobShopInstance,
     Schedule,
@@ -7,8 +9,27 @@ from job_shop_lib import (
 
 
 class Dispatcher:
+    """Handles the logic of scheduling operations on machines.
+
+    Attributes:
+        instance:
+            The instance of the job shop problem to be scheduled.
+        schedule:
+            The schedule of operations on machines.
+        machine_next_available_time:
+            The next available time for each machine.
+        job_next_operation_index:
+            The index of the next operation to be scheduled for each job.
+        job_next_available_time:
+            The next available time for each job.
+    """
 
     def __init__(self, instance: JobShopInstance) -> None:
+        """Initializes the object with the given instance.
+
+        Args:
+            instance: The instance of the job shop problem to be scheduled.
+        """
         self.instance = instance
         self.schedule = Schedule(self.instance)
         self.machine_next_available_time = [0] * self.instance.num_machines
@@ -16,12 +37,30 @@ class Dispatcher:
         self.job_next_available_time = [0] * self.instance.num_jobs
 
     def reset(self) -> None:
+        """Resets the dispatcher to its initial state."""
         self.schedule.reset()
         self.machine_next_available_time = [0] * self.instance.num_machines
         self.job_next_operation_index = [0] * self.instance.num_jobs
         self.job_next_available_time = [0] * self.instance.num_jobs
 
     def dispatch(self, operation: Operation, machine_id: int) -> None:
+        """Schedules the given operation on the given machine.
+
+        The start time of the operation is computed based on the next
+        available time for the machine and the next available time for the
+        job to which the operation belongs. The operation is then scheduled
+        on the machine and the tracking attributes are updated.
+        Args:
+            operation:
+                The operation to be scheduled.
+            machine_id:
+                The id of the machine on which the operation is to be
+                scheduled.
+
+        Raises:
+            ValueError: If the operation is not ready to be scheduled.
+        """
+
         if not self.is_operation_ready(operation):
             raise ValueError("Operation is not ready to be scheduled.")
 
@@ -34,12 +73,35 @@ class Dispatcher:
         self._update_tracking_attributes(scheduled_operation)
 
     def is_operation_ready(self, operation: Operation) -> bool:
+        """Returns True if the given operation is ready to be scheduled.
+
+        An operation is ready to be scheduled if it is the next operation
+        to be scheduled for its job.
+
+        Args:
+            operation:
+                The operation to be checked.
+        """
         return (
             self.job_next_operation_index[operation.job_id]
             == operation.position_in_job
         )
 
     def compute_start_time(self, operation: Operation, machine_id: int) -> int:
+        """Computes the start time for the given operation on the given
+        machine.
+
+        The start time is the maximum of the next available time for the
+        machine and the next available time for the job to which the
+        operation belongs.
+
+        Args:
+            operation:
+                The operation to be scheduled.
+            machine_id:
+                The id of the machine on which the operation is to be
+                scheduled.
+        """
         return max(
             self.machine_next_available_time[machine_id],
             self.job_next_available_time[operation.job_id],
@@ -58,6 +120,13 @@ class Dispatcher:
         self.job_next_available_time[job_id] = end_time
 
     def available_operations(self) -> list[Operation]:
+        """Returns the list of operations that are ready to be scheduled.
+
+        An operation is ready to be scheduled if it is the next operation
+        to be scheduled for its job.
+
+        It is more efficient than checking all operations in the instance.
+        """
         available_operations = []
         for job_id, next_position in enumerate(self.job_next_operation_index):
             if next_position == len(self.instance.jobs[job_id]):
@@ -67,6 +136,13 @@ class Dispatcher:
         return available_operations
 
     def uncompleted_operations(self) -> list[Operation]:
+        """Returns the list of operations that have not been scheduled.
+
+        An operation is uncompleted if it has not been scheduled yet.
+
+        It is more efficient than checking all operations in the instance.
+        """
+
         uncompleted_operations = []
         for job_id, next_position in enumerate(self.job_next_operation_index):
             operations = self.instance.jobs[job_id][next_position:]
