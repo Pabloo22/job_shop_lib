8a1544ec6178ba646c1516400d08c0881c1c6b49
Pabloo22 <pablete.arino@gmail.com>
2024-03-13 16:36:18 +0100

    Add Node class documentation

--- job_shop_lib/graphs/node.py
+++ job_shop_lib/graphs/node.py
@@ -1,3 +1,5 @@
+"""Home of the `Node` class."""
+
 from typing import Optional
 
 from job_shop_lib import Operation
@@ -5,6 +7,55 @@ from job_shop_lib.graphs.constants import NodeType
 
 
 class Node:
+    """Data structure to represent a node in the `JobShopGraph`.
+
+    A node is hashable by its id. The id is assigned when the node is added to
+    the graph. The id must be unique for each node in the graph, and should be
+    used to identify the node in the networkx graph.
+
+    Depending on the type of the node, it can have different attributes. The
+    following table shows the attributes of each type of node:
+
+    Node Type       | Required Attribute
+    ----------------|---------------------
+    OPERATION       | `operation`
+    MACHINE         | `machine_id`
+    JOB             | `job_id`
+
+    In terms of equality, two nodes are equal if they have the same id. 
+    Additionally, one node is equal to an integer if the integer is equal to
+    its id. It is also hashable by its id.
+
+    This allows for using the node as a key in a dictionary, at the same time
+    we can use its id to index that dictionary. Example:
+
+    ```python
+    node = Node(NodeType.SOURCE)
+    node.node_id = 1
+    graph = {node: "some value"}
+    print(graph[node])  # "some value"
+    print(graph[1])  # "some value"
+    ```
+
+    Args:
+        node_type:
+            The type of the node. It can be one of the following:
+            - NodeType.OPERATION
+            - NodeType.MACHINE
+            - NodeType.JOB
+            - NodeType.GLOBAL
+            ...
+        operation:
+            The operation of the node. It should be provided if the node_type
+            is NodeType.OPERATION.
+        machine_id:
+            The id of the machine of the node. It should be provided if the
+            node_type is NodeType.MACHINE.
+        job_id:
+            The id of the job of the node. It should be provided if the
+            node_type is NodeType.JOB.
+    """
+
     __slots__ = "node_type", "_node_id", "_operation", "_machine_id", "_job_id"
 
     def __init__(
@@ -32,6 +83,7 @@ class Node:
 
     @property
     def node_id(self) -> int:
+        """Returns a unique identifier for the node."""
         if self._node_id is None:
             raise ValueError("Node has not been assigned an id.")
         return self._node_id
@@ -42,20 +94,32 @@ class Node:
 
     @property
     def operation(self) -> Operation:
+        """Returns the operation of the node.
+
+        This property is mandatory for nodes of type `OPERATION`.
+        """
         if self._operation is None:
             raise ValueError("Node has no operation.")
         return self._operation
 
     @property
     def machine_id(self) -> int:
+        """Returns the `machine_id` of the node.
+
+        This property is mandatory for nodes of type `MACHINE`.
+        """
         if self._machine_id is None:
-            raise ValueError("Node has no machine_id.")
+            raise ValueError("Node has no `machine_id`.")
         return self._machine_id
 
     @property
     def job_id(self) -> int:
+        """Returns the `job_id` of the node.
+
+        This property is mandatory for nodes of type `JOB`.
+        """
         if self._job_id is None:
-            raise ValueError("Node has no job_id.")
+            raise ValueError("Node has no `job_id`.")
         return self._job_id
 
     def __hash__(self) -> int:
