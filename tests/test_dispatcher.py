dc1f214bba8428388d206e13006a21826c689f83
Pabloo22 <pablete.arino@gmail.com>
2024-04-08 11:03:58 +0200

    [TESTS] Add test_filter_bad_choices function in test_dispatcher.py

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -2,7 +2,17 @@ import pytest
 
 from job_shop_lib import Dispatcher, JobShopInstance
 from job_shop_lib.solvers import DispatchingRuleSolver, DispatchingRule
-from job_shop_lib.benchmarks import load_all_benchmark_instances
+from job_shop_lib.benchmarks import load_benchmark_instance
+
+
+RULES_TO_TEST = [
+    DispatchingRule.MOST_WORK_REMAINING,
+    DispatchingRule.FIRST_COME_FIRST_SERVED,
+    DispatchingRule.MOST_OPERATIONS_REMAINING,
+]
+INSTANCES_TO_TEST = [
+    load_benchmark_instance(f"la{i:02d}") for i in range(1, 41)
+]
 
 
 def test_dispatch(example_job_shop_instance: JobShopInstance):
@@ -122,38 +132,66 @@ def test_uncompleted_operations(example_job_shop_instance: JobShopInstance):
     )
 
 
-# RULES_TO_TEST = [
-#     DispatchingRule.MOST_WORK_REMAINING,
-#     DispatchingRule.FIRST_COME_FIRST_SERVED,
-#     DispatchingRule.MOST_OPERATIONS_REMAINING,
-# ]
+@pytest.mark.parametrize(
+    "dispatching_rule",
+    [rule for rule in DispatchingRule if rule in RULES_TO_TEST],
+)
+@pytest.mark.parametrize("instance", INSTANCES_TO_TEST)
+def test_filter_bad_choices(
+    dispatching_rule: DispatchingRule, instance: JobShopInstance
+):
+    """Test that the optimized solver produces a schedule with a makespan
+    less than or equal to the non-optimized solver.
 
-# @pytest.mark.parametrize(
-#     "dispatching_rule",
-#     [rule for rule in DispatchingRule if rule in RULES_TO_TEST],
-# )
-# @pytest.mark.parametrize("instance", load_all_benchmark_instances().values())
-# def test_filter_bad_choices(
-#     dispatching_rule: DispatchingRule, instance: JobShopInstance
-# ):
-#     optimized_solver = DispatchingRuleSolver(
-#         dispatching_rule=dispatching_rule, filter_bad_choices=True
-#     )
-#     non_optimized_solver = DispatchingRuleSolver(
-#         dispatching_rule=dispatching_rule, filter_bad_choices=False
-#     )
+    It is important to note, though, that there is no theoretical guarantee
+    that the optimized solver will always produce a better schedule. Although
+    the optimized solver always filter out non-optimal choices, removing that
+    option may lead to the solver to choose an even worse option. For example,
+    consider we have three operations A, B, and C, and operation A is the best
+    choice according to the dispatching rule, then B, and finally C. Our
+    opimized scheduler will remove B from the list of choices, however, the
+    solver may choose C instead of A, which would lead to a worse schedule
+    than if it had chosen B. This may result in the optimized solver producing
+    a worse schedule than the non-optimized solver. However, in practice, the
+    optimized solver will usually produce better schedules than the
+    non-optimized solver. From all the benchmarks we have tested, the optimized
+    solver only produced worse schedules in "ta51" and "orb09" instances.
 
-#     optimized_schedule = optimized_solver.solve(instance)
-#     non_optimized_schedule = non_optimized_solver.solve(instance)
-#     optimized_makespan = optimized_schedule.makespan()
-#     non_optimized_makespan = non_optimized_schedule.makespan()
-#     assert optimized_makespan <= non_optimized_makespan, (
-#         f"Optimized makespan: {optimized_makespan}, "
-#         f"Non-optimized makespan: {non_optimized_makespan} "
-#         f"Instance: {instance.name}, "
-#         f"Dispatching rule: {dispatching_rule}"
-#     )
+    A smaller example to illustrate the point:
 
+    ```python
+    instance_dict = {
+        "name": "classic_generated_instance_375",
+        "duration_matrix": [[3, 5, 9],
+                            [6, 1, 6],
+                            [2, 4, 4]],
+        "machines_matrix": [[0, 2, 1],
+                            [2, 0, 1],
+                            [2, 1, 0]],
+        "metadata": {
+            "optimized_makespan": 30,
+            "non_optimized_makespan": 27,
+            "dispatching_rule": "most_work_remaining",
+        },
+    }
+    ```
 
-if __name__ == "__main__":
-    pytest.main(["-v", "test_dispatcher.py"])
+    You can see the plots of the schedules in the `examples` folder.
+    """
+    optimized_solver = DispatchingRuleSolver(
+        dispatching_rule=dispatching_rule, filter_bad_choices=True
+    )
+    non_optimized_solver = DispatchingRuleSolver(
+        dispatching_rule=dispatching_rule, filter_bad_choices=False
+    )
+
+    optimized_schedule = optimized_solver.solve(instance)
+    non_optimized_schedule = non_optimized_solver.solve(instance)
+    optimized_makespan = optimized_schedule.makespan()
+    non_optimized_makespan = non_optimized_schedule.makespan()
+    assert optimized_makespan <= non_optimized_makespan, (
+        f"Optimized makespan: {optimized_makespan}, "
+        f"Non-optimized makespan: {non_optimized_makespan} "
+        f"Instance: {instance.name}, "
+        f"Dispatching rule: {dispatching_rule}"
+    )
