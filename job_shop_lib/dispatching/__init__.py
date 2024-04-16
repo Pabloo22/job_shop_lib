15db8f2904a8d0b2960738395b7b8838855bc85c
Pabloo22 <pablete.arino@gmail.com>
2024-04-16 19:31:13 +0200

    [Refactor] Rename `factories.py` to `dispatching_rules_factories.py`

--- job_shop_lib/dispatching/__init__.py
+++ job_shop_lib/dispatching/__init__.py
@@ -6,7 +6,7 @@ from job_shop_lib.dispatching.dispatching_rules import (
     most_operations_remaining_rule,
     random_operation_rule,
 )
-from job_shop_lib.dispatching.factories import (
+from job_shop_lib.dispatching.dispatching_rules_factories import (
     DispatchingRule,
     MachineChooser,
     dispatching_rule_factory,
