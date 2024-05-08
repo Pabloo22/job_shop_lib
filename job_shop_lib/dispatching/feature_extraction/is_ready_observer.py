d82685b694969a3a44fbc538b00b37cd7a413a5b
Pabloo22 <pablete.arino@gmail.com>
2024-05-08 16:42:33 +0200

    [Refactor] Refactor `IsReadyObserver` to use feature types instead of node types

--- job_shop_lib/dispatching/feature_extraction/is_ready_observer.py
+++ job_shop_lib/dispatching/feature_extraction/is_ready_observer.py
@@ -1,10 +1,8 @@
 """Home of the `IsReadyObserver` class."""
 
-import numpy as np
-
-from job_shop_lib.graphs import NodeType
-from job_shop_lib.dispatching.feature_extraction.feature_observer import (
+from job_shop_lib.dispatching.feature_extraction import (
     FeatureObserver,
+    FeatureType,
 )
 
 
@@ -13,26 +11,17 @@ class IsReadyObserver(FeatureObserver):
     is ready to be dispatched."""
 
     def initialize_features(self):
-        for node_type in self.graph.nodes_by_type:
-            self._initialize_default_node_features(node_type)
-            node_ids = self._get_ready_nodes(node_type)
-            self.node_features[node_type][node_ids] = 1.0
+        for feature_type, feature in self.features.items():
+            node_ids = self._get_ready_nodes(feature_type)
+            feature[node_ids, 0] = 1.0
 
-    def _initialize_default_node_features(self, node_type: NodeType):
-        self.node_features[node_type] = np.zeros(
-            (len(self.graph.nodes_by_type[node_type]), 1),
-            dtype=np.float32,
-        )
-
-    def _get_ready_nodes(self, node_type: NodeType) -> list[int]:
+    def _get_ready_nodes(self, feature_type: FeatureType) -> list[int]:
         mapping = {
-            NodeType.OPERATION: self._get_ready_operation_nodes,
-            NodeType.MACHINE: self.dispatcher.available_machines,
-            NodeType.JOB: self.dispatcher.available_jobs,
+            FeatureType.OPERATIONS: self._get_ready_operation_nodes,
+            FeatureType.MACHINES: self.dispatcher.available_machines,
+            FeatureType.JOBS: self.dispatcher.available_jobs,
         }
-        if node_type not in mapping:
-            return []
-        return mapping[node_type]()
+        return mapping[feature_type]()
 
     def _get_ready_operation_nodes(self) -> list[int]:
         available_operations = self.dispatcher.available_operations()
