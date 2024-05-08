8e4a6434e4bfa2699948809a078ffd30e459ee11
Pabloo22 <pablete.arino@gmail.com>
2024-05-08 16:37:07 +0200

    [Refactor] Decouple `FeatureObserver` class from `JobShopGraph`

--- job_shop_lib/dispatching/feature_extraction/__init__.py
+++ job_shop_lib/dispatching/feature_extraction/__init__.py
@@ -1,4 +1,4 @@
-from .feature_observer import FeatureObserver
+from .feature_observer import FeatureObserver, FeatureType
 from .composite_feature_observer import CompositeFeatureObserver
 from .earliest_start_time_observer import EarliestStartTimeObserver
 from .is_ready_observer import IsReadyObserver
@@ -6,6 +6,7 @@ from .duration_observer import DurationObserver
 
 __all__ = [
     "FeatureObserver",
+    "FeatureType",
     "CompositeFeatureObserver",
     "EarliestStartTimeObserver",
     "IsReadyObserver",
--- job_shop_lib/dispatching/feature_extraction/feature_observer.py
+++ job_shop_lib/dispatching/feature_extraction/feature_observer.py
@@ -2,23 +2,59 @@
 
 from abc import abstractmethod
 
+import enum
+
 import numpy as np
-from job_shop_lib import JobShopLibError, ScheduledOperation
-from job_shop_lib.graphs import JobShopGraph, NodeType
+from job_shop_lib import ScheduledOperation
 from job_shop_lib.dispatching import Dispatcher, DispatcherObserver
+
+
+class FeatureType(str, enum.Enum):
+    """Types of features that can be extracted from a graph."""
+
+    OPERATIONS = "operations"
+    MACHINES = "machines"
+    JOBS = "jobs"
 
 
 class FeatureObserver(DispatcherObserver):
     """Base class for node feature extractors."""
 
-    def __init__(self, dispatcher: Dispatcher, graph: JobShopGraph):
-        if graph is not None and dispatcher.instance is not graph.instance:
-            raise JobShopLibError(
-                "The dispatcher and the graph must have the same instance."
-            )
+    def __init__(
+        self,
+        dispatcher: Dispatcher,
+        feature_types: list[FeatureType] | FeatureType | None = None,
+        feature_size: dict[FeatureType, int] | int = 1,
+    ):
+        if isinstance(feature_types, FeatureType):
+            feature_types = [feature_types]
+        if feature_types is None:
+            feature_types = list(FeatureType)
+        if isinstance(feature_size, int):
+            feature_size = {
+                feature_type: feature_size for feature_type in feature_types
+            }
         super().__init__(dispatcher)
-        self.graph = graph
-        self.node_features: dict[NodeType, np.ndarray] = {}
+
+        number_of_entities = {
+            FeatureType.OPERATIONS: dispatcher.instance.num_operations,
+            FeatureType.MACHINES: dispatcher.instance.num_machines,
+            FeatureType.JOBS: dispatcher.instance.num_jobs,
+        }
+        self.feature_dimensions = {
+            feature_type: (
+                number_of_entities[feature_type],
+                feature_size[feature_type],
+            )
+            for feature_type in feature_types
+        }
+        self.features = {
+            feature_type: np.zeros(
+                self.feature_dimensions[feature_type],
+                dtype=np.float32,
+            )
+            for feature_type in feature_types
+        }
         self.initialize_features()
 
     @abstractmethod
