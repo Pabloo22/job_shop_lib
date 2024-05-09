17da03b0049c2c186374465474c8891da9dd8018
Pabloo22 <pablete.arino@gmail.com>
2024-05-09 18:05:47 +0200

    [Feature]: Add column names to `CompositeFeatureObserver`

--- job_shop_lib/dispatching/feature_extraction/composite_feature_observer.py
+++ job_shop_lib/dispatching/feature_extraction/composite_feature_observer.py
@@ -2,6 +2,7 @@
 
 from collections import defaultdict
 import numpy as np
+import pandas as pd
 
 from job_shop_lib.dispatching import Dispatcher
 from job_shop_lib.dispatching.feature_extraction import (
@@ -13,7 +14,15 @@ from job_shop_lib.dispatching.feature_extraction import (
 class CompositeFeatureObserver(FeatureObserver):
     """Aggregates features from other FeatureObserver instances subscribed to
     the same `Dispatcher` by concatenating their feature matrices along the
-    first axis (horizontal concatenation)."""
+    first axis (horizontal concatenation).
+
+    Attributes:
+        feature_observers:
+            List of `FeatureObserver` instances to aggregate features from.
+        column_names:
+            Dictionary mapping `FeatureType` to a list of column names for the
+            corresponding feature matrix.
+    """
 
     def __init__(
         self,
@@ -27,15 +36,43 @@ class CompositeFeatureObserver(FeatureObserver):
                 if isinstance(observer, FeatureObserver)
             ]
         self.feature_observers = feature_observers
+        self.column_names: dict[FeatureType, list[str]] = defaultdict(list)
         super().__init__(dispatcher)
+        self._set_column_names()
 
     def initialize_features(self):
         features: dict[FeatureType, list[np.ndarray]] = defaultdict(list)
-        for tracker in self.feature_observers:
-            for feature_type, feature_matrix in tracker.features.items():
+        for observer in self.feature_observers:
+            for feature_type, feature_matrix in observer.features.items():
                 features[feature_type].append(feature_matrix)
 
         self.features = {
             feature_type: np.concatenate(features, axis=1)
             for feature_type, features in features.items()
         }
+
+    def _set_column_names(self):
+        for observer in self.feature_observers:
+            for feature_type, feature_matrix in observer.features.items():
+                feature_name = observer.__class__.__name__.replace(
+                    "Observer", ""
+                )
+                if feature_matrix.shape[1] > 1:
+                    self.column_names[feature_type] += [
+                        f"{feature_name}_{i}"
+                        for i in range(feature_matrix.shape[1])
+                    ]
+                else:
+                    self.column_names[feature_type].append(feature_name)
+
+    def __str__(self):
+        out = [f"{self.__class__.__name__}:\n"]
+        out.append("-" * (len(out[0]) - 1))
+        for feature_type, feature_matrix in self.features.items():
+            df = pd.DataFrame(
+                feature_matrix, columns=self.column_names[feature_type]
+            )
+            out.append(
+                (f"\n{feature_type.value} features:\n" f"{df.to_string()}\n")
+            )
+        return "\n".join(out)
