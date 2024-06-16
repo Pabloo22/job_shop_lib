325f4728df951ac997d10cee38f6c91269acf294
Pabloo22 <pablete.arino@gmail.com>
2024-06-16 18:06:58 +0200

    [Refactor] Create `utils.py` file

--- job_shop_lib/reinforcement_learning/__init__.py
+++ job_shop_lib/reinforcement_learning/__init__.py
@@ -1,20 +1,22 @@
 """Package for reinforcement learning components."""
 
-from job_shop_lib.reinforcement_learning.constants import (
-    FeatureObserverConfig,
-    ObservationSpaceKey,
-)
 from job_shop_lib.reinforcement_learning.reward_functions import (
     RewardFunction,
     MakespanReward,
     IdleTimeReward,
 )
+from job_shop_lib.reinforcement_learning.utils import (
+    create_dict_space,
+    create_observation,
+    ObservationSpaceKey,
+)
 
 
 __all__ = [
-    "FeatureObserverConfig",
     "ObservationSpaceKey",
     "RewardFunction",
     "MakespanReward",
     "IdleTimeReward",
+    "create_dict_space",
+    "create_observation",
 ]
