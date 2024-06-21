0c3cb95d4a1007628a9a03741a17667fc1e53d73
Pabloo22 <pablete.arino@gmail.com>
2024-06-21 22:27:25 +0200

    [Feature + Refactor] Support video creation, and create gif from sequence of scheduled operations. File renamed.

--- job_shop_lib/visualization/__init__.py
+++ job_shop_lib/visualization/__init__.py
@@ -1,10 +1,12 @@
 """Package for visualization."""
 
 from job_shop_lib.visualization.gantt_chart import plot_gantt_chart
-from job_shop_lib.visualization.create_gif import (
+from job_shop_lib.visualization.gantt_chart_video_and_gif_creation import (
     create_gif,
+    create_gannt_chart_video,
     create_gantt_chart_frames,
     plot_gantt_chart_wrapper,
+    create_video_from_frames,
     create_gif_from_frames,
 )
 from job_shop_lib.visualization.disjunctive_graph import plot_disjunctive_graph
@@ -15,10 +17,12 @@ from job_shop_lib.visualization.agent_task_graph import (
 
 __all__ = [
     "plot_gantt_chart",
+    "create_gannt_chart_video",
     "create_gif",
     "create_gantt_chart_frames",
     "plot_gantt_chart_wrapper",
     "create_gif_from_frames",
+    "create_video_from_frames",
     "plot_disjunctive_graph",
     "plot_agent_task_graph",
     "three_columns_layout",
