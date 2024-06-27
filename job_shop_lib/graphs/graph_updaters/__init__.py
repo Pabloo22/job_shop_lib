1589d3b94f74eaa6867bf8f0101684649d4d60ed
Pabloo22 <pablete.arino@gmail.com>
2024-06-27 14:19:56 +0200

    [Feature] Add graph updaters to job shop library

---
+++ job_shop_lib/graphs/graph_updaters/__init__.py
@@ -0,0 +1,8 @@
+from .graph_updater import GraphUpdater
+from .utils import remove_completed_operations
+
+
+__all__ = [
+    "GraphUpdater",
+    "remove_completed_operations",
+]
---
+++ job_shop_lib/graphs/graph_updaters/graph_updater.py
@@ -0,0 +1,67 @@
+"""Home of the `GraphBuilderObserver` class."""
+
+from abc import abstractmethod
+from copy import deepcopy
+
+from job_shop_lib import ScheduledOperation
+from job_shop_lib.dispatching import DispatcherObserver, Dispatcher
+from job_shop_lib.graphs import JobShopGraph
+
+
+class GraphUpdater(DispatcherObserver):
+    """Observer that builds and updates a job shop graph.
+
+    This observer uses a provided graph builder function to initialize the
+    job shop graph and a graph updater function to update the graph after
+    each scheduled operation.
+
+    Attributes:
+        dispatcher:
+            The dispatcher instance to observe.
+        graph_initializer:
+            A function that builds the initial job shop graph from a job shop
+            instance.
+        graph_updater:
+            A function that updates the job shop graph based on the
+            dispatcher state and a scheduled operation.
+        job_shop_graph:
+            The job shop graph being managed and updated.
+    """
+
+    def __init__(
+        self,
+        dispatcher: Dispatcher,
+        job_shop_graph: JobShopGraph,
+        *,
+        is_singleton: bool = False,
+        subscribe: bool = True,
+    ):
+        """
+        Args:
+            dispatcher:
+                The dispatcher instance to observe.
+            graph_initializer:
+                A function that builds the initial job shop graph from a job
+                shop instance.
+            graph_updater:
+                A function that updates the job shop graph based on the
+                dispatcher state and a scheduled operation.
+            is_singleton:
+                If True, ensures only one instance of this observer type is
+                subscribed to the dispatcher. Defaults to False.
+            subscribe:
+                If True, automatically subscribes the observer to the
+                dispatcher. Defaults to True.
+        """
+        super().__init__(dispatcher, is_singleton, subscribe)
+        self.initial_job_shop_graph = deepcopy(job_shop_graph)
+        self.job_shop_graph = job_shop_graph
+
+    def reset(self) -> None:
+        """Resets the job shop graph."""
+        self.job_shop_graph = deepcopy(self.initial_job_shop_graph)
+
+    @abstractmethod
+    def update(self, scheduled_operation: ScheduledOperation) -> None:
+        """Updates the job shop graph after an operation has been
+        dispatched."""
