2123999f0c77147ee2d01ec74ce1aab5b9fcde6e
Pabloo22 <pablete.arino@gmail.com>
2024-07-12 21:10:33 +0200

    [Feature] Add `DispatchingRuleObserver`

--- job_shop_lib/dispatching/rules/dispatching_rule_observer.py
+++ job_shop_lib/dispatching/rules/dispatching_rule_observer.py
@@ -2,16 +2,34 @@
 
 from abc import abstractmethod
 from job_shop_lib import Operation
-from job_shop_lib.dispatching import DispatcherObserver
+from job_shop_lib.dispatching import (
+    DispatcherObserver,
+    Dispatcher,
+    MachineChooser,
+    MachineChooserType,
+    machine_chooser_factory,
+)
 
 
-class DispatchingRule(DispatcherObserver):
+class DispatchingRuleObserver(DispatcherObserver):
     """Base class for dispatching rules.
 
     Dispatching rules are used to determine the order in which operations
     should be processed next on a particular machine when that machine becomes
     available.
     """
+
+    def __init__(
+        self,
+        dispatcher: Dispatcher,
+        *,
+        subscribe: bool = True,
+        machine_chooser: (
+            str | MachineChooser | MachineChooserType
+        ) = MachineChooserType.FIRST,
+    ):
+        super().__init__(dispatcher, subscribe=subscribe)
+        self.machine_chooser = machine_chooser_factory(machine_chooser)
 
     @abstractmethod
     def select_action(self) -> tuple[Operation, int]:
