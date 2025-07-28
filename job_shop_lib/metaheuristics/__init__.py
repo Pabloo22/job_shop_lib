6c797e49437f8b1839118bdf8b8454050582635b
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-07-28 15:55:34 +0100

    [Doc]description added for the initializer of the SA module

--- job_shop_lib/metaheuristics/__init__.py
+++ job_shop_lib/metaheuristics/__init__.py
@@ -5,7 +5,11 @@ shop scheduling problems.
 
 SA (Simulated Annealing) is a probabilistic technique (metaheuristic
 algorithm) for approximating the global optimum of a given function.
-
+It seeks to find a good approximation of the optimized makespan
+by exploring the schedule space and accepting worse schedules with
+evaluating the metropolis criterion. It represents the JSSP solution by
+defining the objective function that complies with the constraints of the
+arrival times and deadlines.
 """
 
 from job_shop_lib.metaheuristics._simulated_annealing import JobShopAnnealer
