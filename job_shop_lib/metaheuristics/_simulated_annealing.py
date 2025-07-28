b95f7c18bf9b8e963ac5ecda64e4ac14804063f3
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-07-28 13:30:23 +0100

    [Feature]constructor and initial parameters added

--- job_shop_lib/metaheuristics/_simulated_annealing.py
+++ job_shop_lib/metaheuristics/_simulated_annealing.py
@@ -1,6 +1,6 @@
 import random
 import logging
-from typing import Optional, List
+from typing import Any, Sequence
 
 try:
     from simanneal import Annealer
@@ -10,8 +10,28 @@ except ImportError:
         "Install with: pip install simanneal"
     )
 
+from job_shop_lib import JobShopInstance, Schedule
 
-class JobShopAnnealer:
 
-    def __init__(self):
-        pass
+class JobShopAnnealer(Annealer):
+    """Simulated Annealing implementation from simanneal API
+    for Job Shop Scheduling."""
+
+    def __init__(
+        self,
+        instance: JobShopInstance,
+        initial_state: list[list[int]],
+        penalty_factor: int = 1_000_000,
+    ):
+        """Initializes the annealer with a job shop instance and an initial state.
+
+        Args:
+            instance: The job shop instance to solve.
+            initial_state: Initial state of the schedule as a list of lists,
+                where each sublist represents the operations of a job.
+            penalty_factor: Factor to scale the penalty for infeasible solutions.
+        """
+
+        super().__init__(initial_state)
+        self.instance = instance
+        self.penalty_factor = penalty_factor
