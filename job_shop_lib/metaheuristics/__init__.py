2cc877e7ed63a866952e6e7b9f76da218153ed44
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-07-28 15:45:12 +0100

    [Feature]SA module is initilized for the access within the API

---
+++ job_shop_lib/metaheuristics/__init__.py
@@ -0,0 +1,7 @@
+"""
+Contains the implementation of the JobShopAnnealer class, which is a simulated annealing algorithm
+for solving job shop scheduling problems.
+
+SA (Simulated Annealing) is a probabilistic technique (metaheuristic algorithm) for approximating the global optimum of a given function.
+
+"""
