1b849220f82e6fd53512619f571559433b2f68ed
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-07-28 15:50:04 +0100

    [Feature]JobShopAnnealer class is imported from the metaheuristic directory

--- job_shop_lib/metaheuristics/__init__.py
+++ job_shop_lib/metaheuristics/__init__.py
@@ -1,7 +1,15 @@
 """
-Contains the implementation of the JobShopAnnealer class, which is a simulated annealing algorithm
-for solving job shop scheduling problems.
+Contains the implementation of the JobShopAnnealer class,
+which is a simulated annealing algorithm for solving job
+shop scheduling problems.
 
-SA (Simulated Annealing) is a probabilistic technique (metaheuristic algorithm) for approximating the global optimum of a given function.
+SA (Simulated Annealing) is a probabilistic technique (metaheuristic
+algorithm) for approximating the global optimum of a given function.
 
 """
+
+from job_shop_lib.metaheuristics._simulated_annealing import JobShopAnnealer
+
+__all__ = [
+    "JobShopAnnealer",
+]
