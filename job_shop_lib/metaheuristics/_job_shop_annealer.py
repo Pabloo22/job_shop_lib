066a4b5de4a705e082354b4cdfd7b38200ea315f
NavidCOMSC <amir.navid.rahimi@gmail.com>
2025-07-29 16:03:46 +0100

    [Lint]return type of the method is expanded to None

--- job_shop_lib/metaheuristics/_job_shop_annealer.py
+++ job_shop_lib/metaheuristics/_job_shop_annealer.py
@@ -110,7 +110,7 @@ class JobShopAnnealer(Annealer):
 
     def _get_job_completion_times(
         self, schedule: Schedule
-    ) -> list[Optional[int]]:
+    ) -> list[Optional[int | None]]:
         """Returns the completion time of the last operation for each job."""
         completion_times = [0] * self.instance.num_jobs
         for machine_schedule in schedule.schedule:
