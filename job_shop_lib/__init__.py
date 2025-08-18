ea1a212d7e136800aebf8a8b667accc97164aac9
Pabloo22 <pablete.arino@gmail.com>
2025-08-18 02:04:32 +0200

    [Chore] Bump version to 1.6.0

--- job_shop_lib/__init__.py
+++ job_shop_lib/__init__.py
@@ -19,7 +19,7 @@ from job_shop_lib._schedule import Schedule
 from job_shop_lib._base_solver import BaseSolver, Solver
 
 
-__version__ = "1.5.0"
+__version__ = "1.6.0"
 
 __all__ = [
     "Operation",
