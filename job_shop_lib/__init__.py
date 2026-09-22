29872519e44d690b09523278edbfef24cd9b864d
Pabloo22 <pablete.arino@gmail.com>
2026-09-22 18:49:43 +0200

    [Chore] Update version to 1.7.1

--- job_shop_lib/__init__.py
+++ job_shop_lib/__init__.py
@@ -19,7 +19,7 @@ from job_shop_lib._schedule import Schedule
 from job_shop_lib._base_solver import BaseSolver, Solver
 
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 __all__ = [
     "Operation",
